730085e5b8042b8f6e4f7fa7837ac17edaf08734
Elena <elena.bogacheva97@gmail.com>
2021-05-29 12:06:02 +0300

    rub - rur

--- rest/src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ rest/src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -18,7 +18,8 @@ class MobileBankApiTestV4 {
       // Проверки
       .then()
           .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
       ;
     }
 }
